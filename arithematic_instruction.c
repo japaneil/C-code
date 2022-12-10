b50556f756023ccf48ca9c3a6466ab3face64e1c
Neil Arora <neilarora2008@gmail.com>
2022-12-11 00:40:55 +0530

    new

---
+++ arithematic_instruction.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main()
+{
+    int a = 4;
+    int b = 9;
+    printf("%d\n", a+b);
+    printf("%d\n", a-b);
+    printf("%d\n", a*b);
+    printf("%d", b/a);
+}
---
+++ instructions.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main()
+{
+    int a,b,c,d;
+    a=2;
+    a=b=c=d;
+    printf("%d %d %d %d", a, b, c, d);
+}
