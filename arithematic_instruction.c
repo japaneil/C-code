cf0908edfb1511e23e159bc2ab409750c8bd9fac
Neil Arora <neilarora2008@gmail.com>
2023-07-12 11:55:04 +0530

    Update

--- arithematic_instruction.c
+++ arithematic_instruction.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <math.h> //used for math operations
 
 void main()
 {
     int a = 4;
     int b = 9;
+    float c = pow(a,b);
     printf("%d\n", a+b);
     printf("%d\n", a-b);
     printf("%d\n", a*b);
     printf("%d", b/a);
+    printf("%f", c);
 }
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,4 @@
+int a = 4;
+    int b = 9;
+    float c = pow(a,b);
+    printf("%d\n", a+b);
