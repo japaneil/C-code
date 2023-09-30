4edd52824a164e845006fef75578386d19b07f2a
Neil Arora <neilarora2008@gmail.com>
2023-09-30 13:59:19 +0530

    update

---
+++ 2023/ascii.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+char ch;
+void main(){
+    printf("Please enter the character for which you require the aasci code: ");
+    scanf("%c",&ch);
+    printf("The aasci code for the character you entered is: %d",ch);
+}
