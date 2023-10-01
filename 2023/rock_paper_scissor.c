497c034994b7eb0efdfad65c42a70ed67d16aabf
Neil Arora <neilarora2008@gmail.com>
2023-10-02 00:15:07 +0530

    added game

---
+++ 2023/rock_paper_scissor.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<conio.h>
+char player1;
+char player2;
+char cont;
+void main(){
+   do{
+        printf("Welcome to Rock, Paper and Scissors! Please press enter to continue to the game!");
+        getch();
+        printf("\nPlayer 1 please choose r for rock, p for paper or s for scissors:  ");
+        scanf(" %c",&player1);
+        printf("Player 2 please choose r for rock, p for paper or s for scissors:  ");
+        scanf(" %c",&player2);
+
+        if(player1=='r'){
+            if(player2=='s')
+            printf("Player 1 wins!");
+            else if(player2=='r')
+            printf("It is a draw");
+            else if(player2=='p')
+            printf("Player 2 wins!");
+            else
+            printf("Invalid Input");
+        }
+
+        else if(player1=='p'){
+            if(player2=='s')
+            printf("Player 2 wins!");
+            else if(player2=='p')
+            printf("It is a draw");
+            else
+            printf("Invalid Input");
+        }
+
+        else if(player2=='s'){
+            if(player2=='s')
+            printf("It is a draw");
+            else
+            printf("Invalid Input");
+        }
+
+        else{
+            printf("Invalid Input");
+        }
+
+        printf("\nI hope you enjoyed the game! Please press y to play again : ");
+        scanf(" %c",&cont);
+        getch();
+    } while(cont=='y');
+}
