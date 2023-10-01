2f6150fb08db7845124777295e5338c0d33a44b2
Neil Arora <neilarora2008@gmail.com>
2023-10-01 12:47:29 +0530

    update

--- 2023/calculator.c
+++ 2023/calculator.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
-char type;
+int type;
 float x,y;
 int main(){
     printf("Please enter number 1: ");
     scanf("%f",&x);
     printf("Please enter number 2: ");
     scanf("%f",&y);
-    printf("Please enter what do you want to do with the numbers, insert m for multiplication, insert d for division, insert a for addition and insert s for subtraction: ");
-    scanf("%c",&type);
+    printf("Please enter what do you want to do with the numbers, insert 1 for multiplication, insert 2 for division, insert 3 for addition and insert 4 for subtraction: ");
+    scanf("%d",&type);
 
-    if(type=="109"){
+    if(type==1){
         printf("The result is: %f",x*y);
     }
-    else if(type==""){
+    else if(type==2){
         printf("The result is: %f",x/y);
     }
     else if(type==3){
